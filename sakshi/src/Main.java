b644e594679fb37122e0a21c97ed64c73d83d73a
varun <varun@varun>
2018-01-19 00:00:35 +0530

    starting

---
+++ sakshi/src/Main.java
@@ -0,0 +1,23 @@
+//import statictest.Student;
+//
+//public class Main {
+//
+//	public static void main(String str[]) {
+//
+//		Student st3=new Student(10);
+//	Student st=	new Student(100);
+//	Student st2=new Student(20);
+//	new Student(100);
+//	new Student(102);
+//	new Student(101);
+//	new Student(103);
+//	new Student(103);
+//	Student.getStoreRoll();
+//	/*for(int i= 0 ;i< Student.getStoreRoll().length;i++) {
+//		System.out.println(Student.getStoreRoll()[i]);
+//	}*/
+//	
+//		//System.out.print(Student.);
+//
+//	}
+//}
